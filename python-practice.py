933075447fd769701307c7163b754b9aca023a54
greasemonkey <zachsfreed@icloud.com>
2022-01-20 01:48:27 -0500

    Prossed the try function

--- python-practice.py
+++ python-practice.py
@@ -2,6 +2,7 @@
 import sys
 import socket
 from datetime import datetime
+from unittest import result
 
 #define our target
 if len (sys.argv) ==2: 
@@ -15,6 +16,22 @@ print ("Scanning Target"+target)
 print ("Time Started:" +str(datetime.now))
 print ("-" * 50)
 
+
+try: 
+        for port in range(50,85):
+            s= socket.socker(socket.ap_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result= s.connect_ex((target,port)) #returns an error indecator
+            if result == 0:
+                print("Port {} is open" .format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print ("\nExiting program.")
+    sys.exit()
+except socket.gaierror:
+    print("Hostname could not be resolved")
+    sys.exit()
+except socket.error:
+    print ("Couldn't connect to server")
     
-
-
