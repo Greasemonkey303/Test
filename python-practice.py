e1d0d00395201691ce611edcd1aba27b0d64579f
greasemonkey <zachsfreed@icloud.com>
2022-01-20 01:25:03 -0500

    Bugs fixed at add banner

--- python-practice.py
+++ python-practice.py
@@ -4,10 +4,17 @@ import socket
 from datetime import datetime
 
 #define our target
-if len (sys.argv)
+if len (sys.argv) ==2: 
         target = socket.gethostbyname (sys.argv [1])
 else: 
-    print("Invalid amount of arguments")
-    print(syntax: python3 scanner.py <ip>)
+    print ("Invalid amount of arguments")
+
+#add banner
+print ("-" * 50)
+print ("Scanning Target"+target)
+print ("Time Started:" +str(datetime.now))
+print ("-" * 50)
+
     
+
 
