f11c1068503bafe7310f9dcd3b25a43ab45c88c1
greasemonkey303 <zachsfreed@gmail.com>
2022-01-21 06:14:16 -0500

    password cracker runs great and been formmated

--- ethical-hacking/passwordcracker.py
+++ ethical-hacking/passwordcracker.py
@@ -3,24 +3,24 @@ import hashlib
 from hmac import digest
 from threading import BrokenBarrierError
 from typing import Counter
-
+flag = 0
 pass_hash = input("enter md5 hash!: ")
 
 wordlist = input("file name:")
 
 try:
-    pass_file = open (wordlist, "r")
+    pass_file = open(wordlist, "r")
 except:
     print("No file found")
     quit()
 
 for word in pass_file:
-    
-    enc_wrd =word.encode('utf-8')
-    digest = hashlib.md5(enc_wrd.strip()).hecdigest()
 
-    #print(word)
-    #print(digest)
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+
+    # print(word)
+    # print(digest)
     #print (pass_hash)
 
     if digest == pass_hash:
