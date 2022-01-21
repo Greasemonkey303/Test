4231eb2de4b064c1202e907b7895eb8a211b5fc3
greasemonkey303 <zachsfreed@gmail.com>
2022-01-20 21:25:11 -0500

    NetworkChuck EP 2

--- networkchuck.py
+++ networkchuck.py
@@ -1,2 +1,18 @@
 # /bin/python3
 
+from unicodedata import name
+
+
+print ("Hellow, welcome to NetworkChuck Coffie!!!!")
+
+name = input("What is your Name?\n")
+
+print("Hello " + name + ", Thank you so much for coming in today.\n\n\n")
+
+menu = "Black Coffee, Espresso, Latte, Cappuchino"
+
+print (name + " , what would you like from our mwny today? Here is what we are serving.\n" + menu)
+
+order = input ()
+
+print ("Sounds Good " + name + ", We'll have that " + order + " ready for you in a moment.") 
