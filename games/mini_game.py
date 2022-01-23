5689495265435f4871496158f509c7f20c43c153
greasemonkey303 <zachsfreed@gmail.com>
2022-01-23 07:03:48 -0500

    finnised borders

--- games/mini_game.py
+++ games/mini_game.py
@@ -35,24 +35,24 @@ def draw_window(red, yellow):
 
 # Yellow Player
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a]: #left
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: #left
             yellow.x -= VEL
-    if keys_pressed[pygame.K_d]: #right
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x: #right
             yellow.x += VEL
-    if keys_pressed[pygame.K_w]: #up
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: #up
             yellow.y -= VEL
-    if keys_pressed[pygame.K_s]: #down
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15: #down
             yellow.y += VEL
 
 # Red Player
 def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT]:  #left
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  #left
             red.x -= VEL
-    if keys_pressed[pygame.K_RIGHT]: #right
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH: #right
             red.x += VEL
-    if keys_pressed[pygame.K_UP]:    #up
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:    #up
             red.y -= VEL
-    if keys_pressed[pygame.K_DOWN]:  #down
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:  #down
             red.y += VEL
 
 def main():
