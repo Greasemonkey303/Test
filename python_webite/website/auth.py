4b9d6b80cd4d8a3cfe478e1af9411f9ead06d171
greasemonkey303 <zachsfreed@gmail.com>
2022-01-24 00:17:34 -0500

    3

--- python_webite/website/auth.py
+++ python_webite/website/auth.py
@@ -1,11 +1,11 @@
 from django import views
-from flask import Blueprint
+from flask import Blueprint, render_template 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -13,4 +13,5 @@ def logout():
 
 @auth.route('/sign-up')
 def sigh_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
+
