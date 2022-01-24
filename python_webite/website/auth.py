e7a856670ec96f5b21e170c4c647efe73e83c5dc
greasemonkey303 <zachsfreed@gmail.com>
2022-01-23 23:54:18 -0500

    Finished Html

--- python_webite/website/auth.py
+++ python_webite/website/auth.py
@@ -2,3 +2,15 @@ from django import views
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+@auth.route('/sign-up')
+def sigh_up():
+    return "<p>Sign Up</p>"
