67cb9e3b60b11c1591f1e8ef5e1165f19668cbbf
greasemonkey303 <zachsfreed@gmail.com>
2022-02-20 00:15:03 -0500

    start of part 2

--- mywebsite/mysite/main/views.py
+++ mywebsite/mysite/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(response):
+    return HttpResponse("Zach's Website")
+
+
+def v1(response):
+    return HttpResponse("View 1!")
