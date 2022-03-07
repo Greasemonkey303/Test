e1f0625f290dffd3a1eac24aecff7af056ad4aca
greasemonkey303 <zachsfreed@icloud.com>
2022-03-06 23:18:20 -0500

    end of vid 17 needs work

--- django/react/music_controller/api/models.py
+++ django/react/music_controller/api/models.py
@@ -2,10 +2,6 @@ from django.db import models
 import string
 import random
 
-# Try to fix auto field 
-class Tags(models.Model):
-    id = models.AutoField(primary_key=True)
-# Try to fix auto field 
 
 def generate_unique_code():
     length = 6
@@ -19,10 +15,10 @@ def generate_unique_code():
 
 
 class Room(models.Model):
-    id = models.AutoField(primary_key=True)
     code = models.CharField(
         max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    current_song = models.CharField(max_length=50, null=True)
