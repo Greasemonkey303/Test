2a668f8a54a16aab0fbcae58829f5ba815df159a
greasemonkey303 <zachsfreed@icloud.com>
2022-03-07 22:26:25 -0500

    small change

--- kivy/env/app/main.py
+++ kivy/env/app/main.py
@@ -1,8 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
 
 class MyApp(App):
     def build(self):
-        return Label(text='Zachary Freed')
+        return MyGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
